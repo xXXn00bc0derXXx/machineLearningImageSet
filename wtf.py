ac91f7f1a3027965190e2efb1d785a969eb04581
xXXn00bc0derXXx <52087892+xXXn00bc0derXXx@users.noreply.github.com>
2020-11-24 14:58:15 -0500

    Add files via upload

---
+++ wtf.py
@@ -0,0 +1,39 @@
+import cv2
+import os
+import time
+directory = '/home/evan/huntingApp/videos'
+f = open("dateTimeVideo.txt", "w")
+
+# Function to extract frames
+
+count = 0
+def FrameCapture(path):
+	# Path to video file
+	global count
+	vidObj = cv2.VideoCapture(path)
+	framerate = vidObj.get(cv2.CAP_PROP_FRAME_COUNT)
+	# Used as counter variable
+	# checks whether frames were extracted
+	success = 1
+	while success:
+		# vidObj object calls read
+		# function extract frames
+		success, image = vidObj.read()
+		# Saves the frames with frame-count
+		if success == 1:
+			if count % 30 == 0:
+				cv2.imwrite("frame%d.jpg" % count, image)
+			count += 1
+	
+
+# Driver Code
+if __name__ == '__main__':
+	# Calling the function
+	for entry in os.scandir(directory):
+		if(entry.path.endswith(".mp4")):
+			f.write("%s \n" % time.ctime(os.path.getmtime(entry)))
+			FrameCapture(str(entry.path))
+			#Save single frame from each video
+	f.close()
+
+	
